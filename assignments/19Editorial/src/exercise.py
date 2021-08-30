e54ace6aa3424ecaef00a794629ade3fb704d8ba
fernanda-ponce <89592261+fernanda-ponce@users.noreply.github.com>
2021-08-29 23:38:15 -0500

    Actualicé el problema 5

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,17 @@
+import math
 def main():
     # escribe tu código abajo de esta línea
     pass
+num_palabras = int(input("Dame el número de palabras: "))
+num_paginas = num_palabras/475
+
+if num_palabras < 475:
+    costo = 60 - (60 * 0.10)
+    print("El costo de la publicación es:", costo)
+else:
+    num_paginas = math.ceil (num_palabras/475)
+    costo_descuento = (num_paginas * 60) - ((num_paginas * 60) * 0.10)
+    print("El costo de la publicación es:", costo_descuento)
 
 
 if __name__ == '__main__':
